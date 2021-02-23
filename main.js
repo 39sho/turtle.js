f4367c60516383e5320516734423ad51dd6ee43c
39sho <45312434+39sho@users.noreply.github.com>
2021-02-23 16:34:47 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+import {Turtle, AnimateDrawer, InanimateDrawer} from "./turtle.js";
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 480;
+canvas.height = 360;
+
+const t = new Turtle(new AnimateDrawer(ctx));
+
+const sierpinskiGasket = (size, num) => {
+  for (const _ of Array(3)) {
+    t.forward(size);
+    t.left(120);
+    if (num > 0) {
+      sierpinskiGasket(size / 2, num - 1);
+    }
+  }
+};
+
+t.setPosition(40, 360);
+t.penDown();
+sierpinskiGasket(400, 4);
